af7d22ad4a56087fe7c229e4c678f5075c3c83ab
Abdullah Alabbas <SEabdullahT@gmail.com>
2024-02-13 20:09:40 +0300

    Update Sorter.java

--- Sorter.java
+++ Sorter.java
@@ -19,16 +19,36 @@ public class Sorter {
         }
     }
 
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            
+            arr[j + 1] = key;
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = {5, 2, 8, 1, 9};
         System.out.println("Before sorting: " + Arrays.toString(arr));
         
+        insertionSort(arr);
+
         selectionSort(arr);
         
         System.out.println("After sorting: " + Arrays.toString(arr));
     }
-    
+
     public static void sort(int[] arr) {
         insertionSort(arr);
+
+        selectionSort(arr);
     }
 }
